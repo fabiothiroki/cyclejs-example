adcfa47cb2de3871844bc0f5906736e37df4861d
Fabio Hiroki <fabiothiroki@gmail.com>
2017-04-08 19:46:22 -0300

    Simple http request

--- src/app.js
+++ src/app.js
@@ -2,11 +2,26 @@ import {div} from '@cycle/dom'
 import xs from 'xstream'
 
 export function App (sources) {
-  const vtree$ = xs.of(
-    div('My Awesome Cycle.js app')
-  )
-  const sinks = {
-    DOM: vtree$
-  }
-  return sinks
+
+  const request$ = xs.of(null)
+    .mapTo({
+      url: 'https://swapi.co/api/people/1',
+      category: 'api',
+    });
+
+  const vtree$ = sources.HTTP.select('api')
+    .flatten()
+    .map(res => res.body)
+    .startWith('null')
+    .map(result =>
+      div([
+        `Random number from server: ${result.name}`
+      ])
+    );
+
+
+  return {
+    DOM: vtree$,
+    HTTP: request$
+  };
 }
