fee1e33393d2e7be8ec004780de64faf85ca3652
Fabio Hiroki <fabiothiroki@gmail.com>
2017-04-08 21:47:26 -0300

    Refactor state rendering

--- src/app.js
+++ src/app.js
@@ -3,25 +3,32 @@ import xs from 'xstream'
 
 export function App (sources) {
 
-  const request$ = xs.of(null)
-    .mapTo({
-      url: 'https://swapi.co/api/people/1',
-      category: 'api',
-    });
+  // const searchIntent$ = sources.DOM.select
+
 
   const vtree$ = sources.HTTP.select('api')
     .flatten()
     .map(res => res.body)
     .startWith('null')
-    .map(result =>
-      div([
-        `Random number from server: ${result.name}`
-      ])
-    );
+    .map(renderState);
 
+
+  const request$ = xs.of(null)
+    .mapTo({
+      url: 'https://swapi.co/api/people/1',
+      category: 'api',
+  });
 
   return {
     DOM: vtree$,
     HTTP: request$
   };
+}
+
+function renderState(state) {
+
+  return div([
+    `Random number from server: ${state.name}`
+  ])
+
 }
